6203145e71f6953cfccfb88f4fec46e1754d53f5
kiba <gloirekiba1530@gmail.com>
2022-04-22 13:44:47 +0200

    ♻️ refactor code to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,32 @@
 import Head from "next/head";
-import Link from "next/link";
-import Date from "./components/date";
 import Layout, { siteTitle } from "./components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "./components/date";
+import { GetStaticProps } from "next";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
         <p>
-          Hello my name is <strong>Kiba</strong>, i am a front-end develloper, i create web
-          applications using React and Next.js.
-        </p>
-        {/* <p style={{ color: "red" }}>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p> */}
+        </p>
       </section>
-
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
@@ -40,7 +36,7 @@ export default function Home({ allPostsData }) {
                 <a>{title}</a>
               </Link>
               <br />
-              <small>
+              <small className={utilStyles.lightText}>
                 <Date dateString={date} />
               </small>
             </li>
@@ -50,3 +46,12 @@ export default function Home({ allPostsData }) {
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
